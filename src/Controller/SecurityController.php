9d0b8479fccdb598af70d88256a8ba8a1485cf1b
TheoBourdel <bourdeltheo@gmail.com>
2023-05-23 18:53:33 +0200

    isValid function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,9 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Validator\UserValidator;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
+    private $userValidator;
+
+    public function __construct(UserValidator $userValidator)
+    {
+        $this->userValidator = $userValidator;
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -23,6 +32,7 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
---
+++ src/Validator/UserValidator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Validator;
+
+class UserValidator
+{
+    public function isValid($user)
+    {
+        // Logique de validation de l'utilisateur
+    }
+}
