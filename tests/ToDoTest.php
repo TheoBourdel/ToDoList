db649f9934bc7091964641d0f6c051456a1d9523
TheoBourdel <bourdeltheo@gmail.com>
2023-05-25 09:30:42 +0200

    add test

--- tests/ToDoTest.php
+++ tests/ToDoTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
 use App\Entity\ToDo;
 use App\Entity\Item;
+use App\Entity\User;
 
 class ToDoTest extends TestCase
 {
@@ -24,7 +25,7 @@ class ToDoTest extends TestCase
 
         }
     
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
     
         $item = new Item();
@@ -53,7 +54,7 @@ class ToDoTest extends TestCase
         $item2->setContent("content");
         $item2->setCreationDate($creationDate);
 
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
 
         $todo->addItem($item2);
@@ -68,7 +69,7 @@ class ToDoTest extends TestCase
         $item1->setContent("content");   
         $item1->setCreationDate(new \DateTime());
 
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
 
         $todo->addItem($item1);
@@ -83,7 +84,7 @@ class ToDoTest extends TestCase
         $item->setContent(str_repeat("A", 1001)); 
         $item->setCreationDate(new \DateTime());
 
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
 
         $todo->addItem($item);
@@ -97,7 +98,7 @@ class ToDoTest extends TestCase
         $item->setName('Item 1');
         $item->setCreationDate(new \DateTime());
 
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
         
         $todo->addItem($item);
@@ -108,8 +109,9 @@ class ToDoTest extends TestCase
         $todo = new ToDo();
         $item = new Item();
         $item->setName('Item 1');
-        $item->setContent("content");   
-
+        $item->setContent("content");  
+        
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
 
         $todo->addItem($item);
@@ -130,9 +132,32 @@ class ToDoTest extends TestCase
         $item2->setContent('content'); 
         $item2->setCreationDate(new \DateTime());
 
-        // Vérifie si l'exception est levée
+        // Vérifie si l'exception est levée (assert)
         $this->expectException(\RuntimeException::class);
 
         $todo->addItem($item2);
+    }
+
+    public function testUserInvalid()
+    {
+        $user = new User();
+        $user->setEmail('invalid-email');
+        $user->setFirstName('');
+        $user->setLastName('Doe');
+        $user->setPassword('password');
+        $user->setBirthdate(new \DateTime('-10 years'));
+
+        $todo = new ToDo();
+
+        $item1 = new Item();
+        $item1->setName('Item 1');
+        $item1->setContent('content');
+        $item1->setCreationDate(new \DateTime());
+        $todo->addItem($item1);
+
+        /// Vérifie si l'exception est levée (assert)
+        $this->expectException(\RuntimeException::class);
+
+        $isValid = $user->isValid();
     }
 }
